46d3f81b699f825f649e2c4e56d7db884b1793a3
Philo <philo.mushofi@ai.astra.co.id>
2025-02-06 20:51:26 +0700

    feat::add pinned domains in OkHttpFactory

--- elutility-core/src/main/java/id/elutility/core/network/OkHttpFactory.kt
+++ elutility-core/src/main/java/id/elutility/core/network/OkHttpFactory.kt
@@ -1,17 +1,57 @@
 package id.elutility.core.network
 
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
 
 object OkHttpFactory {
-    fun create(connectTimeout: Long = 30L, readTimeout: Long = 30L, writeTimeout: Long = 30L, interceptors: List<Interceptor> = emptyList()): OkHttpClient {
+
+    private fun builder(
+        connectTimeout: Long = 30L,
+        readTimeout: Long = 30L,
+        writeTimeout: Long = 30L
+    ): OkHttpClient.Builder {
         return OkHttpClient.Builder().apply {
             connectTimeout(connectTimeout, TimeUnit.SECONDS)
             readTimeout(readTimeout, TimeUnit.SECONDS)
             writeTimeout(writeTimeout, TimeUnit.SECONDS)
+        }
+    }
+
+    fun create(
+        connectTimeout: Long = 30L,
+        readTimeout: Long = 30L,
+        writeTimeout: Long = 30L,
+        interceptors: List<Interceptor> = emptyList()
+    ): OkHttpClient {
+        return builder(connectTimeout, readTimeout, writeTimeout).apply {
             interceptors.forEach {
                 addInterceptor(it)
+            }
+        }.build()
+    }
+
+    fun create(
+        connectTimeout: Long = 30L,
+        readTimeout: Long = 30L,
+        writeTimeout: Long = 30L,
+        interceptors: List<Interceptor> = emptyList(),
+        pinnedDomains: Map<String, List<String>> = emptyMap()
+    ): OkHttpClient {
+        return builder(connectTimeout, readTimeout, writeTimeout).apply {
+            interceptors.forEach {
+                addInterceptor(it)
+            }
+            if (pinnedDomains.isNotEmpty()) {
+                val certificatePinner = CertificatePinner.Builder().apply {
+                    pinnedDomains.forEach { (key, hashes) ->
+                        hashes.forEach { pin ->
+                            add(key, pin)
+                        }
+                    }
+                }.build()
+                certificatePinner(certificatePinner)
             }
         }.build()
     }
